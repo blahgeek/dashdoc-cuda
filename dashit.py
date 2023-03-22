72ceff503177c8b7787a047f424b6dc4e6f25136
Simon Boehm <simon@siboehm.com>
2023-03-22 16:34:42 -0700

    Black formatting

--- dashit.py
+++ dashit.py
@@ -3,8 +3,8 @@
 
 # @Author: blahgeek
 # @Date:   2017-02-13
-# @Last modified by:   blahgeek
-# @Last modified time: 2017-10-17
+# @Last modified by:   siboehm
+# @Last modified time: 2023-03-01
 
 
 import re
@@ -17,7 +17,10 @@ from bs4 import BeautifulSoup
 
 
 def remove_navbar(soup):
-    for id_ in ('site-nav', 'resize-nav', ):
+    for id_ in (
+        "site-nav",
+        "resize-nav",
+    ):
         try:
             soup.find(id=id_).extract()
         except AttributeError:
@@ -25,56 +28,58 @@ def remove_navbar(soup):
 
 
 def extract_sectionlink(soup):
-    for link in soup.find_all('div', class_='section-link'):
+    for link in soup.find_all("div", class_="section-link"):
         if not link.a.string:
             continue
-        if re.match(r'^([0-9]+\.)+\s*$', link.a.string, re.M):
+        if re.match(r"^([0-9]+\.)+\s*$", link.a.string, re.M):
             continue
-        yield link.a.string, 'Guide', link.a['href']
+        yield link.a.string, "Guide", link.a["href"]
 
 
 def extract_cppmodule(soup):
-    module = soup.find('div', class_='cppModule')
+    module = soup.find("div", class_="cppModule")
     if not module:
         return
 
     # Enumerations
-    section = soup.find(class_='sectiontitle', text='Enumerations')
+    section = soup.find(class_="sectiontitle", text="Enumerations")
     if section:
-        for item in section.parent.dl.find_all('dt', recursive=False):
-            text = ' '.join(item.stripped_strings)
-            link = item.a['name']
-            yield text.replace('enum ', '').split()[0], 'Enum', link
-            for member in item.find_next_sibling('dd')\
-                    .find_all(class_='enum-member-name-def'):
-                text = ''.join(member.stripped_strings)
-                yield text.partition('=')[0], 'Value', link
+        for item in section.parent.dl.find_all("dt", recursive=False):
+            text = " ".join(item.stripped_strings)
+            link = item.a["name"]
+            yield text.replace("enum ", "").split()[0], "Enum", link
+            for member in item.find_next_sibling("dd").find_all(
+                class_="enum-member-name-def"
+            ):
+                text = "".join(member.stripped_strings)
+                yield text.partition("=")[0], "Value", link
 
     # Functions
-    section = soup.find(class_='fake_sectiontitle', text='Functions')
+    section = soup.find(class_="fake_sectiontitle", text="Functions")
     if section:
-        members = section.find_next_sibling(class_='members')
-        for item in members.find_all(class_='member_name'):
-            yield item.a.string, 'Function', item.a['href']
-        for item in members.find_all(class_='member_name_long_type'):
-            yield item.a.string, 'Function', item.a['href']
+        members = section.find_next_sibling(class_="members")
+        for item in members.find_all(class_="member_name"):
+            yield item.a.string, "Function", item.a["href"]
+        for item in members.find_all(class_="member_name_long_type"):
+            yield item.a.string, "Function", item.a["href"]
 
     # Typedefs
-    section = soup.find(class_='fake_sectiontitle', text='Typedefs')
+    section = soup.find(class_="fake_sectiontitle", text="Typedefs")
     if section:
-        members = section.find_next_sibling(class_='members')
-        for item in members.find_all(class_='member_name'):
-            yield item.a.string, 'Type', item.a['href']
+        members = section.find_next_sibling(class_="members")
+        for item in members.find_all(class_="member_name"):
+            yield item.a.string, "Type", item.a["href"]
 
 
 def make_docset_layout(path):
     shutil.rmtree(path, ignore_errors=True)
-    doc_dir = os.path.join(path, 'Contents/Resources/Documents')
+    doc_dir = os.path.join(path, "Contents/Resources/Documents")
     os.makedirs(doc_dir)
-    shutil.copy(os.path.join(os.path.dirname(__file__), 'icon.png'), path)
-    shutil.copy(os.path.join(os.path.dirname(__file__), 'icon@2x.png'), path)
-    with open(os.path.join(path, 'Contents/Info.plist'), 'w') as plist:
-        plist.write('''<?xml version="1.0" encoding="UTF-8"?>
+    shutil.copy(os.path.join(os.path.dirname(__file__), "icon.png"), path)
+    shutil.copy(os.path.join(os.path.dirname(__file__), "icon@2x.png"), path)
+    with open(os.path.join(path, "Contents/Info.plist"), "w") as plist:
+        plist.write(
+            """<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
 <dict>
@@ -91,26 +96,31 @@ def make_docset_layout(path):
     <key>dashIndexFilePath</key>
     <string>index.html</string>
 </dict>
-</plist>''')
+</plist>"""
+        )
     return doc_dir
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='CUDA Docset generator')
-    parser.add_argument('-s', '--source', help='Source HTML doc folder',
-                        default='/opt/cuda/doc/html/')
-    parser.add_argument('-d', '--dest', help='Destination .docset folder',
-                        default='./CUDA.docset')
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="CUDA Docset generator")
+    parser.add_argument(
+        "-s", "--source", help="Source HTML doc folder", default="/opt/cuda/doc/html/"
+    )
+    parser.add_argument(
+        "-d", "--dest", help="Destination .docset folder", default="./CUDA.docset"
+    )
     args = parser.parse_args()
 
     logging.basicConfig(level=logging.INFO)
 
     doc_dir = make_docset_layout(args.dest)
-    db = sqlite3.connect(os.path.join(doc_dir, '../docSet.dsidx'))
+    db = sqlite3.connect(os.path.join(doc_dir, "../docSet.dsidx"))
     cur = db.cursor()
 
-    cur.execute('''CREATE TABLE searchIndex(id INTEGER PRIMARY KEY, name TEXT,
-                type TEXT, path TEXT);''')
+    cur.execute(
+        """CREATE TABLE searchIndex(id INTEGER PRIMARY KEY, name TEXT,
+                type TEXT, path TEXT);"""
+    )
 
     for dirpath, dirnames, filenames in os.walk(args.source):
         relpath = os.path.relpath(dirpath, args.source)
@@ -118,37 +128,40 @@ if __name__ == '__main__':
         for filename in filenames:
             filepath = os.path.join(args.source, relpath, filename)
             dstpath = os.path.join(doc_dir, relpath, filename)
-            logging.info('Processing file {}'.format(filepath))
+            logging.info("Processing file {}".format(filepath))
 
             indexs = list()
             try:
-                assert filepath.endswith('.html')
-                with open(filepath, 'rb') as src:
-                    soup = BeautifulSoup(src.read().decode('utf8'), 'lxml')
+                assert filepath.endswith(".html")
+                with open(filepath, "rb") as src:
+                    soup = BeautifulSoup(src.read().decode("utf8"), "lxml")
                     for index in extract_cppmodule(soup):
                         indexs.append(index)
                     for index in extract_sectionlink(soup):
                         indexs.append(index)
                     remove_navbar(soup)
-                    with open(dstpath, 'wb') as dst:
-                        dst.write(soup.encode('utf8'))
+                    with open(dstpath, "wb") as dst:
+                        dst.write(soup.encode("utf8"))
             except AssertionError:
                 # except:
                 shutil.copy(filepath, dstpath)
 
             for name, typ, pos in indexs:
-                name = re.sub('\s+', ' ', name)
-                assert '\n' not in name
-                if '#' in pos[1:] or '/' in pos:
+                name = re.sub("\s+", " ", name)
+                assert "\n" not in name
+                if "#" in pos[1:] or "/" in pos:
                     continue
                 # print(name, typ, pos)
                 # assert('#' not in pos[1:])
-                if not pos.startswith('#'):
-                    pos = '#' + pos
+                if not pos.startswith("#"):
+                    pos = "#" + pos
                 pos = os.path.join(relpath, filename) + pos
-                cur.execute('INSERT OR IGNORE INTO searchIndex(name,type,path) '
-                            'VALUES (?, ?, ?);', (name, typ, pos))
+                cur.execute(
+                    "INSERT OR IGNORE INTO searchIndex(name,type,path) "
+                    "VALUES (?, ?, ?);",
+                    (name, typ, pos),
+                )
 
-    cur.execute('CREATE UNIQUE INDEX anchor ON searchIndex (name,type,path);')
+    cur.execute("CREATE UNIQUE INDEX anchor ON searchIndex (name,type,path);")
     db.commit()
     db.close()
